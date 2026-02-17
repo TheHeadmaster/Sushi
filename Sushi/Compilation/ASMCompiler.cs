ba19f936a9a7fee5c1d505cea1eef792929f94f5
PALMER, DAVID S IV CTR USAF ACC 690 CYCS CYSA (688 CW A6CD) <david.palmer.41.ctr@us.af.mil>
2026-02-17 17:26:21 -0600

    Added ASMCompiler.

---
+++ Sushi/Compilation/ASMCompiler.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Sushi.Compilation;
+
+public static class ASMCompiler
+{
+    public static async Task Compile()
+    {
+    }
+}
