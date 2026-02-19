27c25e596d565eee27e299c5e3c64a446420f724
David Palmer <davidivpalmer@gmail.com>
2026-02-19 00:24:34 -0600

    Renamed Generate to TryGenerate and added documentation comments.

--- Sushi/Lexing/Tokenization/TokenGenerator.cs
+++ Sushi/Lexing/Tokenization/TokenGenerator.cs
@@ -1,10 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Sushi.Lexing.Tokenization;
 
+/// <summary>
+/// A token generator handles the generation of a specific type of token or tokens.
+/// Tokens will be handled based on highest affinity.
+/// </summary>
 public abstract class TokenGenerator
 {
-    public abstract Task<TokenGeneratorResult> Generate(string source, long index);
+    /// <summary>
+    /// Tries to generate a token for the specific <see cref="TokenFile"/>.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public abstract Task<TokenGeneratorResult> TryGenerate(string source, long index);
 }
