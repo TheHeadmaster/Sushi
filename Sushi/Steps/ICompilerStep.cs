3740079374eb90d884cf72e0339f01302692d061
David Palmer <davidivpalmer@gmail.com>
2026-02-19 00:05:00 -0600

    Added ICompilerStep.

---
+++ Sushi/Steps/ICompilerStep.cs
@@ -0,0 +1,23 @@
+﻿namespace Sushi.Steps;
+
+/// <summary>
+/// Represents a step of the compiler that handles a specific part of the compilation process.
+/// </summary>
+public interface ICompilerStep
+{
+    /// <summary>
+    /// Initializes the <see cref="ICompilerStep"/>.
+    /// </summary>
+    /// <returns>
+    /// An awaitable <see cref="Task"/>.
+    /// </returns>
+    public Task Initialize();
+
+    /// <summary>
+    /// Runs the <see cref="ICompilerStep"/>.
+    /// </summary>
+    /// <returns>
+    /// An awaitable <see cref="Task"/>.
+    /// </returns>
+    public Task Run();
+}
