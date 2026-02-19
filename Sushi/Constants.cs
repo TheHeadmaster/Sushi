8c7112e022a61893f4fda255be1dca24d83375d0
David Palmer <davidivpalmer@gmail.com>
2026-02-19 00:44:23 -0600

    Added Constants class.

---
+++ Sushi/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Sushi;
+
+/// <summary>
+/// Holds constants used accross the entire application.
+/// </summary>
+public static class Constants
+{
+    /// <summary>
+    /// Contains keywords reserved by the language, and therefore cannot be used as identifiers.
+    /// </summary>
+    public static ReadOnlyCollection<string> ReservedKeywords { get; } =
+    [
+
+    ];
+}
