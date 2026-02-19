4cf5d79ab55fa760faa1892c2fece82c81af3b69
TheHeadmaster <45703202+TheHeadmaster@users.noreply.github.com>
2026-02-19 11:21:48 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,27 @@
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+project = 'Sushi'
+author = 'TheHeadmaster'
+version = release = '0.1'
+
+# -- General configuration ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+]
+extensions = []
+language = 'en'
+master_doc = 'index'
+pygments_style = 'sphinx'
+source_suffix = '.rst'
+templates_path = ['_templates']
+
+# -- Options for HTML output ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
