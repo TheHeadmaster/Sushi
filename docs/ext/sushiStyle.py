628d90f3a79de7d4120e1a86cbeb0aa574bbeeba
PALMER, DAVID S IV CTR USAF ACC 690 CYCS CYSA (688 CW A6CD) <davidivpalmer@gmail.com>
2026-02-25 18:08:13 -0600

    Changed to asterisk import for simplicity.

--- docs/ext/sushiStyle.py
+++ docs/ext/sushiStyle.py
@@ -1,6 +1,5 @@
 from pygments.style import Style
-from pygments.token import Token, Comment, Keyword, Name, String, \
-     Error, Generic, Number, Operator
+from pygments.token import *
 
 class SushiLexerStyle(Style):
 
