20d39141e3663444bc8a74152a98c897e942ba1c
PALMER, DAVID S IV CTR USAF ACC 690 CYCS CYSA (688 CW A6CD) <davidivpalmer@gmail.com>
2026-02-26 12:05:59 -0600

    Adjusted syntax highlighting.

--- docs/ext/sushi.py
+++ docs/ext/sushi.py
@@ -1,17 +1,21 @@
 # sushi.py
-from pygments.lexer import RegexLexer
-from pygments.token import Token
+import re
+
+from pygments.lexer import RegexLexer, bygroups, include, using, this, default, words
+from pygments.token import *
 
 class SushiLexer(RegexLexer):
     name = 'Sushi'
-    aliases = ['sushi']
+    aliases = ['sushi', 'sus']
+    filenames = ['*.sus']
+
+    flags = re.MULTILINE | re.DOTALL
+
     tokens = {
         'root': [
-            (r'(?s)\/\*(?!\*\/)(.*)\*\/', Token.Comment),
-            (r'//.*', Token.Comment),
-            (r'My', Token.Keyword),
+            (r'//.*?\n', Token.Comment.Single),
+            (r'/[*].*?[*]/', Token.Comment.Multiline),
+            (r'(int32|float32|bool|true|false)', Token.Keyword),
             (r'[a-zA-Z_]\w*', Token.Name),
-            (r'\s', Token.Text),
-            (r'.', Token.Operator),
         ]
     }
