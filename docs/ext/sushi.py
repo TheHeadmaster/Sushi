e44e25bab57f1ffc9def0380526a4f0564d9bf7d
David Palmer <davidivpalmer@gmail.com>
2026-02-25 21:36:09 -0600

    Attempting to fix broken lexing.

--- docs/ext/sushi.py
+++ docs/ext/sushi.py
@@ -1,15 +1,15 @@
 # sushi.py
 from pygments.lexer import RegexLexer
-from pygments import token
+from pygments.token import Token
 
 class SushiLexer(RegexLexer):
     name = 'Sushi'
     aliases = ['sushi']
     tokens = {
         'root': [
-            (r'MyKeyword', token.Keyword),
-            (r'[a-zA-Z_]\w*', token.Name),
-            (r'\s', token.Text),
-            (r'.', token.Operator),
+            (r'MyKeyword', Token.Keyword),
+            (r'[a-zA-Z_]\w*', Token.Name),
+            (r'\s', Token.Text),
+            (r'.', Token.Operator),
         ]
     }
