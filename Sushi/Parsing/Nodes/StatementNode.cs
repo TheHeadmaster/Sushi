ea727c678ff18cf27a61b2177d97bedfb1a1e7f0
PALMER, DAVID S IV CTR USAF ACC 690 CYCS CYSA (688 CW A6CD) <davidivpalmer@gmail.com>
2026-03-16 16:43:58 -0500

    Removed unnecessary using statements.

--- Sushi/Parsing/Nodes/StatementNode.cs
+++ Sushi/Parsing/Nodes/StatementNode.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
-using Sushi.Compilation;
-
 namespace Sushi.Parsing.Nodes;
 
 public abstract class StatementNode : SyntaxNode
--- Sushi/Parsing/Nodes/TypeNode.cs
+++ Sushi/Parsing/Nodes/TypeNode.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 using Sushi.Tokenization;
 using Sushi.Verification;
 
