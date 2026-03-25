3189abff065db2f13b2a559a8429139a0453c9aa
PALMER, DAVID S IV CTR USAF ACC 690 CYCS CYSA (688 CW A6CD) <davidivpalmer@gmail.com>
2026-03-25 09:22:03 -0500

    Added CreatorDeclarationNode and CreatorDeclarationParser. Fixed a documentation typo in DestroyerDeclarationNode.

--- Sushi/Parsing/Nodes/CreatorDeclarationNode.cs
+++ Sushi/Parsing/Nodes/CreatorDeclarationNode.cs
@@ -4,24 +4,24 @@ using Sushi.Tokenization;
 namespace Sushi.Parsing.Nodes;
 
 /// <summary>
-/// Represents a destroyer declaration.
+/// Represents a creator declaration.
 /// </summary>
 /// <param name="token">
 /// The token that is the start of the declaration.
 /// </param>
 /// <param name="name">
-/// The name of the destroyer.
+/// The name of the creator.
 /// </param>
 /// <param name="parameterList">
 /// The list of parameters.
 /// </param>
 /// <param name="body">
-/// The body of the destroyer.
+/// The body of the creator.
 /// </param>
-public sealed class DestroyerDeclarationNode([NotNull] Token token, IdentifierNode? name, ParameterListNode? parameterList, BlockNode? body) : StatementNode
+public sealed class CreatorDeclarationNode([NotNull] Token token, IdentifierNode? name, ParameterListNode? parameterList, BlockNode? body) : StatementNode
 {
     /// <summary>
-    /// The name of the destroyer.
+    /// The name of the creator.
     /// </summary>
     public IdentifierNode? Name { get; set; } = name;
 
@@ -31,7 +31,7 @@ public sealed class DestroyerDeclarationNode([NotNull] Token token, IdentifierNo
     public ParameterListNode? ParameterList { get; set; } = parameterList;
 
     /// <summary>
-    /// The body of the destroyer.
+    /// The body of the creator.
     /// </summary>
     public BlockNode? Body { get; set; } = body;
 
