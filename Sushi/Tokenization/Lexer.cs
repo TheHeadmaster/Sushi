4469bd5e3fcdb1cefcc83dcd301af35f281d1f4e
PALMER, DAVID S IV CTR USAF ACC 690 CYCS CYSA (688 CW A6CD) <davidivpalmer@gmail.com>
2026-04-06 15:24:54 -0500

    Made LexFile public and added LexStringAsFileText.

--- Sushi/Tokenization/Lexer.cs
+++ Sushi/Tokenization/Lexer.cs
@@ -58,10 +58,27 @@ public sealed partial class Lexer
     /// <returns>
     /// An awaitable <see cref="Task"/> that returns the converted <see cref="TokenFile"/>.
     /// </returns>
-    private static async Task<TokenFile> LexFile([NotNull] string sourceFilePath)
+    public static async Task<TokenFile> LexFile([NotNull] string sourceFilePath)
     {
         string source = await File.ReadAllTextAsync(sourceFilePath);
 
+        TokenFile file = new()
+        {
+            FileName = Path.GetFileName(sourceFilePath),
+            FilePath = sourceFilePath,
+            RawSourceCode = source
+        };
+
+        while (!file.LastIndexOfFileReached())
+        {
+            await ConsumeTokenWithHighestAffinity(file);
+        }
+
+        return file;
+    }
+
+    public static async Task<TokenFile> LexStringAsFileText(string source, string sourceFilePath)
+    {
         TokenFile file = new()
         {
             FileName = Path.GetFileName(sourceFilePath),
