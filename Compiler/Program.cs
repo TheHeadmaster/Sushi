36f7bfa68b9ba232a10f752a011bad30a3211a02
TheHeadmaster <davidivpalmer@gmail.com>
2026-09-17 14:25:33 -0500

    Added Program.cs.

---
+++ Compiler/Program.cs
@@ -0,0 +1,19 @@
+
+namespace Sushi;
+
+/// <summary>
+/// Contains the application entry point and top-level application lifecycle.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// The application entry point.
+    /// </summary>
+    /// <param name="args">
+    /// The command line arguments.
+    /// </param>
+    private static void Main(string[] args)
+    {
+        
+    }
+}
