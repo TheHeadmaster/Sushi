6cb086ed520d2bfccd00f860cadfb621ab043912
TheHeadmaster <davidivpalmer@gmail.com>
2026-09-18 01:37:58 -0500

    Added stubs for the LSP. did some slight reshuffling of Program.cs methods. Added OmniSharp package.

---
+++ Compiler/SushiLanguageService.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Sushi;
+
+/// <summary>
+/// Service that handles the coordination of lexing, parsing, updating, and compiling of Sushi code bases.
+/// </summary>
+public sealed class SushiLanguageService
+{
+    /// <summary>
+    /// Initializes the language service with the information about the currently open workspace. Only used in LSP mode.
+    /// </summary>
+    /// <param name="workspaceFolders">
+    /// The folders that are a part of the workspace.
+    /// </param>
+    /// <returns>
+    /// An awaitable <see cref="Task"/>.
+    /// </returns>
+    public async Task InitializeWorkspace([NotNull] IEnumerable<WorkspaceFolder> workspaceFolders)
+    {
+        // Stub
+    }
+
+    /// <summary>
+    /// Runs a compile job using the current settings. This runs the whole lexing, parsing, and compilation stack.
+    /// Only used when not running in LSP mode.
+    /// </summary>
+    /// <returns>
+    /// An awaitable <see cref="Task"/>.
+    /// </returns>
+    public async Task CompileJob()
+    {
+        // Stub
+    }
+}
